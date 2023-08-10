3e0fb7fcdfc0fe796a9e28647aa8a7cb81a9fb3d
Rudraksh225 <rudrakshjani03@gmail.com>
2023-08-10 14:19:39 +0530

    Added react top loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -6,18 +6,31 @@ import { Provider } from 'react-redux'
 import { useEffect, useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import LoadingBar from 'react-top-loading-bar'
+import Router, { useRouter }  from 'next/router'
 
 export default function App({ Component, pageProps }) {
 
   const [user, setUser] = useState(null)
   const [key, setKey] = useState(null)
+  const [progress, setProgress] = useState(0)
 
+  const router = useRouter();
   useEffect(() => {
+
+    //Top loading bar progress set to 100 when switch routing complete
+    router.events.on('routeChangeComplete', ()=>{
+      setProgress(50)
+    })
+
+    router.events.on('routeChangeComplete', ()=>{
+      setProgress(100)
+    })
+
     const token = localStorage.getItem('token');
     if(token){
       setUser(token);
       setKey(Math.random())
-      
     }
   },[])
 
@@ -40,6 +53,11 @@ export default function App({ Component, pageProps }) {
 
   return <>
     <Provider store={store}>
+      <LoadingBar
+        color='#2432f0'
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
       <Navbar logout={logout} user={user} key={key} />
       <Component {...pageProps} />
       <Footer/>
